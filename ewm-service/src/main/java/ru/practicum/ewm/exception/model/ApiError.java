bad6bc1e565369edcfc7a546fb3ad85285e2a11a
kpetrosyan1 <karen-petrosyan1996@yandex.ru>
2024-05-06 22:19:02 +0400

    ДП2: добавить метод ToString к ApiError

--- ewm-service/src/main/java/ru/practicum/ewm/exception/model/ApiError.java
+++ ewm-service/src/main/java/ru/practicum/ewm/exception/model/ApiError.java
@@ -2,11 +2,13 @@ package ru.practicum.ewm.exception.model;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.AllArgsConstructor;
+import lombok.Data;
 
 import java.time.LocalDateTime;
 import java.util.List;
 
 @AllArgsConstructor
+@Data
 public class ApiError {
     private List<String> errors;
     private String message;
